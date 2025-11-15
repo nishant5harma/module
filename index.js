8e1258404255c96f1a2ad82fcacff55da2f1fd6a
Nishant Sharma <95848188+nishant5harma@users.noreply.github.com>
2025-11-15 16:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 export default {
-    script: '<script src="https://cdn.jsdelivr.net/npm/ezyapi@latest/ezyapi.min.js"></script>',
-    script_url: 'https://cdn.jsdelivr.net/npm/ezyapi@latest/ezyapi.min.js',
-    status: 'success',
-    timestamp: 'STATIC_FROM_GITHUB'
+    script: '<script src="https://cdn.jsdelivr.net/npm/ezyapi@latest/ezyapi.min.js"></script>'
 };
