4ce0dd10cf6981c3db0ed3262cb0e35169c8109e
Nishant Sharma <95848188+nishant5harma@users.noreply.github.com>
2025-11-15 16:29:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export default {
+    script: '<script src="https://cdn.jsdelivr.net/npm/ezyapi@latest/ezyapi.min.js"></script>',
+    script_url: 'https://cdn.jsdelivr.net/npm/ezyapi@latest/ezyapi.min.js',
+    status: 'success',
+    timestamp: 'STATIC_FROM_GITHUB'
+};
